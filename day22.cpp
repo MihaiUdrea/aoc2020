5a6b43edb4113d0553363ea4915e85d278f5e01a
Mihai Udrea <mihai.udrea@caphyon.com>
2020-12-22 15:30:06 +0200

    ranges power

--- day22.cpp
+++ day22.cpp
@@ -25,7 +25,8 @@ namespace
 {
 struct Solve
 {
-  map<int, std::list<int>> mycards;
+  using list = std::list<int>;
+  vector<list> mycards {2};
 
   Solve(const string & inStr){
    for (auto [idx, chunk] : inStr | views::tokenize(regex("\n\n"), -1) | views::transform(to_string_view()) | views::enumerate)
@@ -33,131 +34,43 @@ struct Solve
         mycards[idx].push_back(stoi(line));
   };
 
-  using list = std::list<int>;
-
-  pair<int,int> Play(map<int, std::list<int>> cards)
+  pair<int,int> Play(vector<list> cards, bool recursive = false)
   {
-    while (none_of(cards | views::values, &list::empty))
+    set<vector<list>> history;
+
+    int winnerIdx = 0;
+    while (none_of(cards, &list::empty))
     {
-      auto rgn = cards | views::values;
+      if (!history.insert(cards).second)
+        return { 0, 0 };
 
-      auto  minMax   = minmax_element(cards, less{}, [](auto & l) {
-        return l.second.front();
-      });
+      auto HasSpace = [](const auto & deck) {return deck.front() < (int)deck.size();};
 
-      auto & winer    = minMax.max->second;
-      auto & loser    = minMax.min->second;
+      winnerIdx = recursive && all_of(cards, HasSpace) 
+        ? Play(cards 
+          | views::transform([](auto & deck) { 
+              return deck 
+                | views::tail 
+                | views::take(deck.front()) 
+                | to<std::list>;}) 
+          | to<vector>, recursive).first
+        : cards[1].front() > cards[0].front();
+      
+      auto & winner = cards[winnerIdx];
+      auto & loser = cards[1 - winnerIdx];
 
-      winer.push_back(winer.front());
-      winer.push_back(loser.front());
+      winner.push_back(winner.front());
+      winner.push_back(loser.front());
 
-      winer.pop_front();
-      loser.pop_front();
-
-      /** /
-      if (cards[0].front() > cards[1].front())
-      {
-        cards[0].push_back(cards[0].front());
-        cards[0].push_back(cards[1].front());
-
-        cards[0].pop_front();
-        cards[1].pop_front();
-      }
-      else
-      {
-        cards[1].push_back(cards[1].front());
-        cards[1].push_back(cards[0].front());
-
-        cards[0].pop_front();
-        cards[1].pop_front();
-      }
-      /**/
+      winner.pop_front();
+      loser.pop_front();    
     }
 
-    int idx = 0;
-    if (cards[0].empty())
-      idx = 1;
-
-
-    return { idx, inner_product(cards[idx] | views::reverse, views::iota(1), 0) }; 
+    return { winnerIdx, inner_product(cards | views::join | views::reverse, views::iota(1), 0) }; 
   }
 
   string Do() { return to_string(Play(mycards).second); }
-  string Do2() { return to_string(Play2(mycards).second); }
-  
-
-  pair<int, int> Play2(map<int, std::list<int>> cards, int level = 0)
-  {
-    set<pair<std::list<int>, std::list<int>>> history;
-
-    
-    int round = 0;
-    while (!cards[0].empty() && !cards[1].empty())
-    {
-      round++;
-
-      if (!history.insert({ cards[0], cards[1] }).second)
-        return { 0, 0 };
-
-      //cout << "Round :" << round << "minigame:" << level << std::endl;
-
-       bool zeroWins = false;
-       if (cards[0].front() < cards[0].size() && cards[1].front() < cards[1].size())
-       {
-         map<int, std::list<int>> cardsMinigame;
-         cardsMinigame[0] = cards[0] | views::tail | views::take(cards[0].front()) | to<std::list>;
-         cardsMinigame[1] = cards[1] | views::tail | views::take(cards[1].front()) | to<std::list>;
-         zeroWins         = Play2(std::move(cardsMinigame), level + 1).first == 0;
-
-         //cout << "DECIDED BY MINIGAME\n";
-       }
-       else
-       {
-         zeroWins = cards[0].front() > cards[1].front();
-       }
-       
-      if (zeroWins)
-      {
-        //cout << "Player 1 wins\n";
-        cards[0].push_back(cards[0].front());
-        cards[0].push_back(cards[1].front());
-
-        cards[0].pop_front();
-        cards[1].pop_front();
-      }
-      else
-      {
-        //cout << "Player 2 wins\n";
-        cards[1].push_back(cards[1].front());
-        cards[1].push_back(cards[0].front());
-
-        cards[0].pop_front();
-        cards[1].pop_front();
-      }
-
-      if (0)
-      {
-      cout << "Cards state:\nPlayer 1: ";
-      for (auto i : cards[0])
-        cout << i << " ";
-      cout << "\nPlayer 2: ";
-      for (auto i : cards[1])
-        cout << i << " ";
-      cout << std::endl;
-      }      
-    }
-
-    int idx = 0;
-    if (cards[0].empty())
-      idx = 1;
-
-    int nr = 0;
-    for (auto [pos, card] : cards[idx] | views::enumerate)
-      nr += (cards[idx].size() - pos) * card;
-
-    
-    return { idx, nr };   
-  }
+  string Do2() { return to_string(Play(mycards, true).second); }
 };
 }  // namespace
 
@@ -250,7 +163,7 @@ TEST_CASE(TODAY "Part One", HIDE_IF_OLD_TEST "[x.]")
 #endif  // FIRST_STAR
 }
 
-TEST_CASE(TODAY "Part Two", HIDE_IF_OLD_TEST "[.]")
+TEST_CASE(TODAY "Part Two", HIDE_IF_OLD_TEST "[x.]")
 {
 #ifdef SECOND_STAR
   REQUIRE(Solve(ReadFileToString(TODAY "input.txt")).Do2() == SECOND_STAR);
